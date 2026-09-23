5f874c10aa6c1f3913bf791b38af8f0a944a1038
sg0176734-svg <sg0176734@gmail.com>
2026-09-23 17:38:38 +0530

    Fix auth controller filename case

--- server/routes/authroutes.js
+++ server/routes/authroutes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const {
   registerUser,
   loginUser,
-} = require("../controllers/authcontroller");
+} = require("../controllers/authController");
 
 const router = express.Router();
 
